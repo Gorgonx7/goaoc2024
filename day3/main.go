d7c10b3f664684d2f0bc5d1063e11dd7d45001cd
James Gordon <james.gordon2@ibm.com>
2024-12-04 08:58:32 +0000

    day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -10,22 +10,29 @@ import (
 
 func main() {
 	data := readData("data.txt")
-	rx := regexp.MustCompile(`mul\([0-9]+,[0-9]+\)`)
-	matches := rx.FindAllString(data, -1)
+	data = fmt.Sprintf("do()%sdon't()", data)
+	dosDonts := regexp.MustCompile(`do\(\)(.*?)don't\(\)|do\(\)(.*?)$|^(.*?)do\(\)`)
+	doDontMatches := dosDonts.FindAllString(data, -1)
 	total := 0
-	for _, match := range matches {
-		parts := strings.Split(match, ",")
-		part1, err := strconv.Atoi(strings.TrimPrefix(parts[0], "mul("))
-		if err != nil {
-			panic(err)
+	for _, doMatch := range doDontMatches {
+		rx := regexp.MustCompile(`mul\([0-9]+,[0-9]+\)`)
+		matches := rx.FindAllString(doMatch, -1)
+
+		for _, match := range matches {
+			parts := strings.Split(match, ",")
+			part1, err := strconv.Atoi(strings.TrimPrefix(parts[0], "mul("))
+			if err != nil {
+				panic(err)
+			}
+			part2, err := strconv.Atoi(strings.TrimSuffix(parts[1], ")"))
+			if err != nil {
+				panic(err)
+			}
+			total += part1 * part2
 		}
-		part2, err := strconv.Atoi(strings.TrimSuffix(parts[1], ")"))
-		if err != nil {
-			panic(err)
-		}
-		total += part1 * part2
 	}
-	fmt.Printf("part 1: %v\n", total)
+
+	fmt.Printf("part 2: %v\n", total)
 
 }
 
