24b9a9807ef3d8a0ab723e476fb64a921d63e1a3
James Gordon <james.gordon2@ibm.com>
2024-12-04 11:40:11 +0000

    day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -28,102 +28,125 @@ func main() {
 	total := 0
 	for line := range lines {
 		for char := range len(lines[line]) {
-			if SolveForDirection(target, UP, char, line, lines) {
-				total += 1
+			if lines[line][char] == "A"[0] {
+				rightDiagonalOK := false
+				if SolveForDirection("M", UP_LEFT, char, line, lines) {
+					if SolveForDirection("S", DOWN_RIGHT, char, line, lines) {
+						rightDiagonalOK = true
+					}
+				} else if SolveForDirection("S", UP_LEFT, char, line, lines) {
+					if SolveForDirection("M", DOWN_RIGHT, char, line, lines) {
+						rightDiagonalOK = true
+					}
+				}
+				leftDiagonalOK := false
+				if SolveForDirection("M", UP_RIGHT, char, line, lines) {
+					if SolveForDirection("S", DOWN_LEFT, char, line, lines) {
+						leftDiagonalOK = true
+					}
+				} else if SolveForDirection("S", UP_RIGHT, char, line, lines) {
+					if SolveForDirection("M", DOWN_LEFT, char, line, lines) {
+						leftDiagonalOK = true
+					}
+				}
+				if rightDiagonalOK && leftDiagonalOK {
+					total += 1
+				}
 			}
-			if SolveForDirection(target, DOWN, char, line, lines) {
-				total += 1
-			}
-			if SolveForDirection(target, LEFT, char, line, lines) {
-				total += 1
-			}
-			if SolveForDirection(target, RIGHT, char, line, lines) {
-				total += 1
-			}
-			if SolveForDirection(target, UP_LEFT, char, line, lines) {
-				total += 1
-			}
-			if SolveForDirection(target, UP_RIGHT, char, line, lines) {
-				total += 1
-			}
-			if SolveForDirection(target, DOWN_LEFT, char, line, lines) {
-				total += 1
-			}
-			if SolveForDirection(target, DOWN_RIGHT, char, line, lines) {
-				total += 1
-			}
+			// if SolveForDirection(target, UP, char, line, lines) {
+			// 	total += 1
+			// }
+			// if SolveForDirection(target, DOWN, char, line, lines) {
+			// 	total += 1
+			// }
+			// if SolveForDirection(target, LEFT, char, line, lines) {
+			// 	total += 1
+			// }
+			// if SolveForDirection(target, RIGHT, char, line, lines) {
+			// 	total += 1
+			// }
+			// if SolveForDirection(target, UP_LEFT, char, line, lines) {
+			// 	total += 1
+			// }
+			// if SolveForDirection(target, UP_RIGHT, char, line, lines) {
+			// 	total += 1
+			// }
+			// if SolveForDirection(target, DOWN_LEFT, char, line, lines) {
+			// 	total += 1
+			// }
+			// if SolveForDirection(target, DOWN_RIGHT, char, line, lines) {
+			// 	total += 1
+			// }
 		}
 	}
 	fmt.Printf("total: %v\n", total)
 }
 
 func SolveForDirection(target string, direction Direction, char, line int, lines []string) bool {
-	if len(target) == 1 {
-		return lines[line][char] == target[0]
-	}
 	switch direction {
 	case RIGHT:
 		if char+1 > len(lines[line])-1 {
 			return false
 		}
-		if lines[line][char] == target[0] {
-			return SolveForDirection(target[1:], RIGHT, char+1, line, lines)
+		if lines[line][char+1] == target[0] {
+			return true
 		}
 		return false
 	case LEFT:
-		if lines[line][char] == target[0] {
-			if char-1 < 0 {
-				return false // Edge handling
-			}
-			return SolveForDirection(target[1:], LEFT, char-1, line, lines)
+		if char-1 < 0 {
+			return false // Edge handling
+		}
+		if lines[line][char-1] == target[0] {
+			return true
 		}
 		return false
 	case UP:
-		if lines[line][char] == target[0] {
-			if line-1 < 0 {
-				return false // Edge handling
-			}
-			return SolveForDirection(target[1:], UP, char, line-1, lines)
+		if line-1 < 0 {
+			return false // Edge handling
+		}
+		if lines[line-1][char] == target[0] {
+
+			return true
 		}
 		return false
 	case DOWN:
-		if lines[line][char] == target[0] {
-			if line+1 > len(lines)-1 {
-				return false // Edge handling
-			}
-			return SolveForDirection(target[1:], DOWN, char, line+1, lines)
+		if line+1 > len(lines)-1 {
+			return false // Edge handling
+		}
+		if lines[line+1][char] == target[0] {
+			return true
 		}
 		return false
 	case UP_LEFT:
-		if lines[line][char] == target[0] {
-			if line-1 < 0 || char-1 < 0 {
-				return false // Edge handling
-			}
-			return SolveForDirection(target[1:], UP_LEFT, char-1, line-1, lines)
+		if line-1 < 0 || char-1 < 0 {
+			return false // Edge handling
+		}
+		if lines[line-1][char-1] == target[0] {
+			return true
 		}
 		return false
 	case UP_RIGHT:
-		if lines[line][char] == target[0] {
-			if line-1 < 0 || char+1 > len(lines[line])-1 {
-				return false // Edge handling
-			}
-			return SolveForDirection(target[1:], UP_RIGHT, char+1, line-1, lines)
+		if line-1 < 0 || char+1 > len(lines[line])-1 {
+			return false // Edge handling
+		}
+		if lines[line-1][char+1] == target[0] {
+			return true
 		}
 		return false
 	case DOWN_RIGHT:
-		if lines[line][char] == target[0] {
-			if line+1 > len(lines)-1 || char+1 > len(lines[line])-1 {
-				return false // Edge handling
-			}
-			return SolveForDirection(target[1:], DOWN_RIGHT, char+1, line+1, lines)
+		if line+1 > len(lines)-1 || char+1 > len(lines[line])-1 {
+			return false // Edge handling
+		}
+		if lines[line+1][char+1] == target[0] {
+			return true
 		}
 		return false
 	case DOWN_LEFT:
-		if lines[line][char] == target[0] {
-			if line+1 > len(lines)-1 || char-1 < 0 {
-				return false
-			}
-			return SolveForDirection(target[1:], DOWN_LEFT, char-1, line+1, lines)
+		if line+1 > len(lines)-1 || char-1 < 0 {
+			return false
+		}
+		if lines[line+1][char-1] == target[0] {
+			return true
 		}
 		return false
 	default:
