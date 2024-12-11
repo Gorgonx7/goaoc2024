971e89650cf2df2b2da88f26cf5e64234911ab59
James Gordon <james.gordon2@ibm.com>
2024-12-11 08:56:20 +0000

    day 5 part 2

--- day5/main.go
+++ day5/main.go
@@ -16,12 +16,14 @@ func main() {
 	total := 0
 	for _, line := range strings.Split(instructions, "\n") {
 		if recordIsGood(rules, line) {
-			num, err := strconv.Atoi(getMiddleNumber(line))
-			if err != nil {
-				panic(err)
-			}
-			total += num
+			continue
 		}
+		line = fixRecord(rules, line)
+		num, err := strconv.Atoi(getMiddleNumber(line))
+		if err != nil {
+			panic(err)
+		}
+		total += num
 	}
 	fmt.Println(total)
 }
@@ -31,6 +33,40 @@ func getMiddleNumber(record string) string {
 		panic(fmt.Errorf("even numbered list, what the *$|^?"))
 	}
 	return nums[len(nums)/2.0]
+}
+func fixRecord(rules map[string]bool, record string) string {
+	splitRecord := strings.Split(record, ",")
+
+RESTART:
+	record = strings.Join(splitRecord, ",")
+	if recordIsGood(rules, record) {
+		return record
+	}
+	for i := range splitRecord {
+		if i == 0 {
+			continue
+		}
+		for j := range make([]int, i) {
+			if ruleExists(rules, string(splitRecord[i]), string(splitRecord[j])) {
+				store := make([]string, len(splitRecord))
+				copy(store, splitRecord)
+				result := make([]string, 0)
+				result = append(result, store[:j]...)
+				result = append(result, store[i])
+				result = append(result, store[j])
+				if i != len(store)-1 {
+					store = append(store[:i], store[i+1:]...)
+				} else {
+					store = store[:i]
+				}
+				result = append(result, store[j+1:]...)
+
+				copy(splitRecord, result)
+				goto RESTART
+			}
+		}
+	}
+	panic(fmt.Errorf("reached end and shouldn't have"))
 }
 func recordIsGood(rules map[string]bool, record string) bool {
 	splitRecord := strings.Split(record, ",")
--- day5/main_test.go
+++ day5/main_test.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_buildRules(t *testing.T) {
-	tests := []struct {
-		name       string
-		rulesBlock string
-		want       map[int][]int
-	}{
-		{
-			name:       "test parse",
-			rulesBlock: "55|42\n55|81\n51|11\n51|22",
-			want: map[int][]int{
-				55: {42, 81},
-				51: {11, 22},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := buildRules(tt.rulesBlock); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("buildRules() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func Test_buildRules(t *testing.T) {
+// 	tests := []struct {
+// 		name       string
+// 		rulesBlock string
+// 		want       map[int][]int
+// 	}{
+// 		{
+// 			name:       "test parse",
+// 			rulesBlock: "55|42\n55|81\n51|11\n51|22",
+// 			want: map[int][]int{
+// 				55: {42, 81},
+// 				51: {11, 22},
+// 			},
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := buildRules(tt.rulesBlock); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("buildRules() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
 func Test_getMiddleNumber(t *testing.T) {
 	tests := []struct {
@@ -126,6 +127,60 @@ func Test_recordIsGood(t *testing.T) {
 			if got := recordIsGood(tt.rules, tt.record); got != tt.want {
 				t.Errorf("recordIsGood() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_fixRecord(t *testing.T) {
+	globalRules := map[string]bool{
+		"47|53": true,
+		"97|13": true,
+		"97|61": true,
+		"97|47": true,
+		"75|29": true,
+		"61|13": true,
+		"75|53": true,
+		"29|13": true,
+		"97|29": true,
+		"53|29": true,
+		"61|53": true,
+		"97|53": true,
+		"61|29": true,
+		"47|13": true,
+		"75|47": true,
+		"97|75": true,
+		"47|61": true,
+		"75|61": true,
+		"47|29": true,
+		"75|13": true,
+		"53|13": true,
+	}
+
+	tests := []struct {
+		name   string
+		record string
+		want   string
+	}{
+		{
+			name:   "one",
+			record: "75,97,47,61,53",
+			want:   "97,75,47,61,53",
+		},
+		{
+			name:   "two",
+			record: "61,13,29",
+			want:   "61,29,13",
+		},
+		{
+			name:   "three",
+			record: "97,13,75,29,47",
+			want:   "97,75,47,29,13",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := fixRecord(globalRules, tt.record)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
